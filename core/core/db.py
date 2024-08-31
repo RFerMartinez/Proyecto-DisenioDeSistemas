4f834e6a25103e0402cc5f6027b9c8d8ceb62d70
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-08-31 15:22:11 -0300

    Se creó la estructura de los directorios del proyecto.

---
+++ core/core/db.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configuración para DB por defecto (sqlite3)
+SQILTE3 = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Configuración para DB PostgreSQL
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'FerBD42276',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
