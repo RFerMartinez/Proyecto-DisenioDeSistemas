d0b19120dbdec366b86ff6087732ea5c6541d867
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-08-31 15:53:28 -0300

    Se actualizó .gitignore

---
+++ core/core/db.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configuración para DB por defecto (sqlite3)
+SQILTE3 = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+print("hola")
+
+# Configuración para DB PostgreSQL
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nombre_de_base_de_datos',
+        'USER': 'postgres',
+        'PASSWORD': 'contraseña_base_de_datos',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
