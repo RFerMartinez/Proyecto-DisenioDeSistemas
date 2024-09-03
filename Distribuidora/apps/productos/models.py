9a7584cb422edf3754902089a8d8ba39a3ce59cd
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-09-03 18:42:09 -0300

    Se creó el modelo de 'Producto'

--- Distribuidora/apps/productos/models.py
+++ Distribuidora/apps/productos/models.py
@@ -15,5 +15,24 @@ class Producto(models.Model):
     cantidad_minima = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name='Cantidad Máxima')
     estado = models.BooleanField(default=True)
 
-    # Claves foráneas a las tablas 'Categoría' y 'Unidad'
-    categoria = models.BooleanField(default=True)
+    # relaciones a las tablas 'Categoría' y 'Unidad'
+    categoria = models.ForeignKey(Categoria, related_name='productos', on_delete=models.CASCADE)
+
+    # La función '__str__' sirve para representar un objeto de la tabla, en lugar de 'object (1)'
+    def __str__(self):
+        return f"{self.codigo} - {self.nombre}"
+    
+    # Para acceder a los 'metadatos' o comportamiento adicional de la table (no de los campos)
+    class Meta:
+        db_table = 'Producto'
+        ordering = ['nombre'] # ['-precio'] -> esto es para ordenar de manera descendente
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        '''
+        Atributos adicionales:
+        abstract = True -> para que la clase sea 'abstracta'
+        permissions = [
+            ('puede_ver_reporte')
+        ]
+        
+        '''
