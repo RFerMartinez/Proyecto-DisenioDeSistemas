58d25e2aed7154ba3a6190fd15224bf63722f378
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-09-03 18:44:25 -0300

    Se creó la tabla 'Categoría'

--- Distribuidora/apps/categorias/models.py
+++ Distribuidora/apps/categorias/models.py
@@ -4,3 +4,11 @@ class Categoria(models.Model):
     nombre = models.CharField(max_length=20, verbose_name='Nombre de Categoría')
     estado = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.nombre}"
+    
+    class Meta:
+        db_table = 'Categoria'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['nombre']
