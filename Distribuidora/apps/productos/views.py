7183d401d869def990ff248808619656e1b31af4
RFerMartinez <ricardofernandomartinez0@gmail.com>
2024-09-04 17:02:38 -0300

    Haciendo view para listar productos

--- Distribuidora/apps/productos/views.py
+++ Distribuidora/apps/productos/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -16,6 +17,27 @@ def listar_productos(request):
         'productos' : productos,
     }
     return render(request, template, context=ctx)
+
+# VISTA basada en clase para poder listar los productos y paginarlos
+class ListarProductos(ListView):
+    # Definir el template para renderizar la lista
+    template_name = 'productos/listar.html'
+    # Definir el modelo sobre el cual se trabajará (para las querys)
+    model = Producto
+    # Definir la cantidad de productos por páginas
+    paginate_by = 10
+    # Definir como se hará referenca a los productos desde el template (via context)
+    context_object_name = 'productos'
+
+    # Método para agregar datos adicioanles al contexto
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        '''Realizar lógica y/o agregar datos al contexto. Realizar filtros'''
+    
+    # Definir/modificar la consulta
+    def get_queryset(self):
+        return Producto.objects.all().order_by('nombre')
+
 
 # VISTA basada en clase para poder crear un nuevo producto
 class CrearProducto(CreateView):
